69dc38e00b0f1eb34a1ef361de9fe8f2596df31d
Jindra Vavruska <radio.miskovice@gmail.com>
2016-09-22 13:28:21 +0200

    Tested methods truncate(), addSeconds(), addMinutes(), addHours(), addDays(), addMonths(), addYears()

--- smart-date.js
+++ smart-date.js
@@ -31,25 +31,34 @@ class SmartDate extends Date {
                 this.getHours(), this.getMinutes(), this.getSeconds() ] ;
         switch( precision ) {
             case 'year'  : ;
-            case 'YYYY'  : date.MM = 0 ;
+            case 'YYYY'  : MM = 0 ;
             case 'month' :
-            case 'MM'    : date.DD = 1 ;
+            case 'MM'    : DD = 1 ;
             case 'day'   :
-            case 'DD'    : date.hh = 0 ;
+            case 'DD'    : hh = 0 ;
             case 'hour'  :
-            case 'hh'    : date.mm = 0 ;
+            case 'hh'    : mm = 0 ;
             case 'minute' : 
-            case 'mm'    : date.ss = 0 ;
+            case 'mm'    : ss = 0 ;
+            case 'ss'    : 
+            case 'second': 
                 return new SmartDate( YYYY, MM, DD, hh, mm, ss );
-                break ;
             default: throw new Error (`Invalid precision code in SmartDate.truncate( precision ). Precision code = "${precision}"`) ;
         }
     }
 
+    addSeconds ( seconds ) { let newDate = new Date(this); newDate.setSeconds( newDate.getSeconds() + seconds ); return newDate ; }
+    addMinutes ( minutes ) { let newDate = new Date(this); newDate.setMinutes( newDate.getMinutes() + minutes ); return newDate ; }
+    addHours   ( hours )   { let newDate = new Date(this); newDate.setHours( newDate.getHours() + hours ); return newDate ; }
+    addDays    ( days )    { let newDate = new Date(this); newDate.setDate( newDate.getDate() + days ); return newDate ; }
+    addMonths  ( months )  { let newDate = new Date(this); newDate.setMonth( newDate.getMonth() + months ); return newDate ; }
+    addYears   ( years )   { let newDate = new Date(this); newDate.setFullYear( newDate.getFullYear() + years ); return newDate ; }
+    
     firstDay() {
         let result = new SmartDate( this.getFullYear(), this.getMonth(), 1 );
         return result ;
     }
+
     lastDay() {
 
     }    
